bdbe41d500182f55620f798eab5d214bdf826a42
Simon <simon.wallin@eyevinn.se>
2017-05-09 14:15:55 +0200

    replaced \ with /

--- config.py
+++ config.py
@@ -1,2 +1,2 @@
-NER_path = ("NER\stanford-ner-2014-06-16\stanford-ner-2014-06-16\classifiers\english.all.3class.distsim.crf.ser.gz",
-"NER\stanford-ner-2014-06-16\stanford-ner-2014-06-16\stanford-ner.jar")
+NER_path = ("NER/stanford-ner-2014-06-16/stanford-ner-2014-06-16/classifiers/english.all.3class.distsim.crf.ser.gz",
+"NER/stanford-ner-2014-06-16/stanford-ner-2014-06-16/stanford-ner.jar")
